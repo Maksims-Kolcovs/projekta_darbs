a2ff31060f04ed7a5d392b86570eaa6d593f821d
Daniēls Pauls Savickis <138810785+danielspaulssavickis@users.noreply.github.com>
2024-01-18 16:10:18 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,61 +1,108 @@
-import selenium
 import csv
+import pandas as pd
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
-from tkinter import simpledialog, Tk
+import tkinter as tk
+from tkinter import simpledialog, filedialog, messagebox
 
-root = Tk()
+root = tk.Tk()
 root.withdraw()
 
+# Logs, kurā var ielikt video url 
+
+video_url = simpledialog.askstring("Ievade", "Lūdzu ievadiet video URL linku:", parent=root)
+
+
+# Selenium inicializēšana
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
-wait = WebDriverWait(driver, 30)
+driver.maximize_window()
+wait = WebDriverWait(driver, 15)
 
-url = "https://www.youtube.com/watch?v=7ESeQBeikKs"
 
-driver.get(url)
+driver.get(video_url)
 sleep(5)
 
-driver.find_element(By.XPATH, "//*[@id='content']/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button").click()
-sleep(1)
 
+# Uzspiež, ka apstiprina cookies 
+
+driver.find_element(By.XPATH, "//*[@id='content']/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button").click()
+sleep(3)
+
+
+
+# Uzspiež "...more pogu" 
 try:
-    element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='expand']")))
-    element.click()
+    more_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='expand']")))
+    more_button.click()
 except Exception as e:
     print(f"An error occurred: {e}")
 
-sleep(1)
+sleep(3)
 
+
+
+# Spiež "Show Transcript pogu" 
 try:
     show_transcript = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='primary-button']/ytd-button-renderer/yt-button-shape/button")))
     show_transcript.click()
 except Exception as e:
     print(f"An error occured: {e}")
 
+
 sleep(3)
 
-try:
-    element2 = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='body']/ytd-transcript-segment-list-renderer")))
-    actions = ActionChains(driver)
-    actions.move_to_element(element2).perform()
-    transcript = element2.text
-    print(transcript)
-except Exception as e:
-    print(f"An error occurred: {e}")
+
+
+# Atrod transkripta logu un nokopē visu tekstu 
+
+transcript_element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='body']/ytd-transcript-segment-list-renderer")))
+transcript = transcript_element.text
+print(transcript)
+
+
+# Pieļaujam, ka transkripts ir string rinda ar laika norādi un tekstu.
+
+# Sagrupē rindas kopā kā laika norāde un teksta pāros
 
 lines = transcript.strip().split('\n')
+
+# Sagrupē rindas (timestamp, text) pāros
 transcript_pairs = [(lines[i], lines[i+1]) for i in range(0, len(lines), 2)]
 
-with open('C:/Users/maksi/transcript.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Timestamp", "Text"])  
-    writer.writerows(transcript_pairs)
+
+file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+
+# Saglabā šīs rindu pārus CSV failā
+if file_path:
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Timestamp", "Text"])  # Uzraksta kolonnas virsrakstus Timestamp un Text
+        writer.writerows(transcript_pairs)  # Pieraksta laika norādes un teksta rindas
+
+# Piedāvā saglabāt excel formātā
+prompt_for_excel = messagebox.askyesno(title="Apstiprinājums", message="Vai Jūs vēlaties saglabāt transkriptu arī Microsoft Excel formātā?")
+
+if prompt_for_excel:
+    transcript_sadalits = transcript.split('\n')
+    transcript_timestamps = transcript_sadalits[::2]
+
+    transcript_text = transcript_sadalits[1::2]
+    df = pd.DataFrame({'timestamp':transcript_timestamps,'text':transcript_text})
+    
+    file_path = filedialog.asksaveasfilename(defaultextension=".xlsx")
+
+    if file_path:
+        # Saglabā DataFrame Excel failī
+        df.to_excel(file_path, index=False)
+
+        messagebox.showinfo(title="Apstiprinājums", message="Fails saglabāts excel formātā.")
+
+
+# Apstiprinājuma logs
+end_window_resultbox = messagebox.showinfo(title="Apstiprinājums", message="Transkripts veiksmīgi saglabāts.")
